db7c057ca18045993c455b90e1fb5aea61d8ddf6
Roman <jorgerromanb@gmail.com>
2022-05-25 16:44:13 -0500

    Primer commit de los ejercicios de JavaScript

---
+++ js/main.js
@@ -0,0 +1,33 @@
+//Solicitar 3 números (entre el 1 y el 100)  y definir cual es el mayor
+// let a= prompt("Escribe el primer numero (entre el 1 y el 100): ");
+// let b= prompt("Escribe el segundo numero (entre el 1 y el 100): ");
+// let c= prompt("Escribe el tercer numero (entre el 1 y el 100): ");
+// if ((a>b)&&(a>c)){console.log("El numero mayor es", a);}
+// else if (b>c){console.log("El numero mayor es", b);}
+// else {console.log("El numero mayor es", c);}
+
+// //Solicitar 3 números (entre el 1 y el 100)  y definir el menor de tres números
+// if ((a<b)&&(a<c)){console.log("El numero menos es", a);}
+// else if (b<c){console.log("El numero menos es", b);}
+// else {console.log("El numero menos es", c);}
+
+//Solicitar un entero (entre el 100 y el 200) y determinar si es multiplo de 3
+
+// let a= prompt("Escribe un numero entero (entre el 100 y 200)");
+// if ((a=>100)&&(a<=200)) {}
+// if (a%3==0){console.log("Tu numero es multiplo de 3");}
+// else {console.log("No es multiplo de 3")};
+
+//leer 3 números y determinar sí uno es la suma de los otros dos
+// let a= prompt("Escribe el primer numero");
+// let b= prompt("Escribe el segundo numero");
+// let c= prompt("Escribe el tercer numero");
+// if (a+b==c){console.log("El tercer numero es la suma de los otros");}
+// else if (a+c==b){console.log("El segundo numero es la suma de los otros");}
+// else if(c+b==a){console.log("El primer numero es la suma de los otros");}
+// else {console.log("Ninguno es la suma de los otros");}
+
+//leer un número y determinar si es par o impar
+// let a= prompt("Escribe un numero");
+// if (a%2==0){console.log("Tu numero es par");}
+// else{console.log("Tu numero es impar");}
